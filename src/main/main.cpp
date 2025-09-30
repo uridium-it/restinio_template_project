77af50f14b2ab82745db4c65f478901d1a322eae
alex <alessandro.bresci@uridium.it>
2025-09-30 11:55:28 +0200

    first commit

---
+++ src/main/main.cpp
@@ -0,0 +1,13 @@
+
+#include <restinio/core.hpp>
+int main()
+{
+    restinio::run(
+        restinio::on_this_thread()
+        .port(8080)
+        .address("localhost")
+        .request_handler([](auto req) {
+            return req->create_response().set_body("Hello, World!").done();
+        }));
+    return 0;
+}
